2a06b0a2fa2be17f3bc2572c3910de7ccb90debb
Christian Berger <christian@pebl-cic.co.uk>
2025-08-05 13:04:49 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,9 +1,8 @@
 
 "use server"
 
-import 'dotenv/config';
 import { suggestVideoTag, type SuggestVideoTagInput } from "@/ai/flows/suggest-video-tag"
-import { adminDb, adminStorage } from "@/lib/firebase-admin";
+import { adminStorage } from "@/lib/firebase-admin";
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "./firebase";
 import type { Video } from "./types";
